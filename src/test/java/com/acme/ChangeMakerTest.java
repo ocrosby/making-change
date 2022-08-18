4994f4712a98daecc1ee29b53b81bc641882da02
Omar Crosby <omar.crosby@gmail.com>
2022-08-18 10:03:29 -0400

    feat(change): Adding another test

--- src/test/java/com/acme/ChangeMakerTest.java
+++ src/test/java/com/acme/ChangeMakerTest.java
@@ -9,9 +9,15 @@ import org.junit.Test;
  */
 public class ChangeMakerTest {
     @Test
-    public void shouldAnswerWithTheExpectedValue() {
+    public void shouldAnswerWithTheExpectedValue_137() {
         String expected = "5 quarters, 1 dimes, 0 nickels, 2 pennies";
-        ChangeMaker changeMaker = new ChangeMaker();
-        assertEquals(expected, changeMaker.makeChange(137));
+        assertEquals(expected, ChangeMaker.makeChange(137));
     }
+
+    @Test
+    public void shouldAnswerWithTheExpectedValue_100() {
+        String expected = "4 quarters, 0 dimes, 0 nickels, 0 pennies";
+        assertEquals(expected, ChangeMaker.makeChange(100));
+    }
+
 }
