96edbc5a57779f766cfc7ffbc33b00866ac94bd5
Omar Crosby <omar.crosby@gmail.com>
2022-08-18 10:39:01 -0400

    feat(change): Adding a test for negative numbers

--- src/test/java/com/denmark/ChangeMakerTest.java
+++ src/test/java/com/denmark/ChangeMakerTest.java
@@ -22,4 +22,9 @@ public class ChangeMakerTest {
         assertEquals(expected, ChangeMaker.makeChange(100));
     }
 
+    @Test
+    public void shouldAnswerWithTheExpectedValue_negative() {
+        String expected = "0 quarters, 0 dimes, 0 nickels, 0 pennies";
+        assertEquals(expected, ChangeMaker.makeChange(-20));
+    }    
 }
